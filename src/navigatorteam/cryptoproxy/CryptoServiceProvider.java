139b11c4481c57eef378086de88ebe7e03ab7675
Giuseppe Petrosino <parsleyjoe@gmail.com>
2019-07-22 14:26:10 +0200

    update crypto interfaces

--- src/navigatorteam/cryptoproxy/CryptoServiceProvider.java
+++ src/navigatorteam/cryptoproxy/CryptoServiceProvider.java
@@ -3,7 +3,7 @@ package navigatorteam.cryptoproxy;
 /**
  * Created on 2019-07-22.
  */
-public interface CryptoServiceProvider<K> {
-    String encrypt(String input, K key);
-    String decrypt(String input, K key);
+public interface CryptoServiceProvider {
+    String encrypt(String input);
+    String decrypt(String input);
 }
--- src/navigatorteam/cryptoproxy/DummyCrypto.java
+++ src/navigatorteam/cryptoproxy/DummyCrypto.java
@@ -3,14 +3,15 @@ package navigatorteam.cryptoproxy;
 /**
  * Created on 2019-07-22.
  */
-public class DummyCrypto implements CryptoServiceProvider<Object> {
+public class DummyCrypto implements CryptoServiceProvider {
+
     @Override
-    public String encrypt(String input, Object key) {
+    public String encrypt(String input) {
         return input;
     }
 
     @Override
-    public String decrypt(String input, Object key) {
+    public String decrypt(String input) {
         return input;
     }
 }
